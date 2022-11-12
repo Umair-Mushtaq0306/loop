562b51f15071a91d5c404aa26d91ddd978e626f0
Umair <Umairmushtaq0345.uma@gmail.com>
2022-11-12 13:24:06 +0500

    first commit

--- loop.dart
+++ loop.dart
@@ -1,6 +1,8 @@
-void  main() {
+import 'dart:io';
+
+// void  main() {
 //  int counter = 0;
-  // counter = counter +1;
+//   counter = counter +1;
 // counter++    // post increament
 // print(counter);
 // int abc = counter++ + ++counter + counter-- - ++counter + ++counter + counter-- - --counter;
@@ -9,19 +11,145 @@ void  main() {
 
 // var list = ["Bilal","Ali","Maaz", "Ahmed"];
 // for (var i=0; i<3;i++)
-var table = 5;
-for(var a = 1;a<=15;a++){
-print("$table x $a = ${a*table}");
+// var table = 5;
+// var table = (int.parse(stdin.readLineSync()!));
+// for(var a = 1;a<=15;a++){
+// print("$table x $a = ${a*table}");}
 // print(list);
 // print(list[i]);
 // print(list[1]);
+// }
+// var GeeksForGeeks = [1,2,3,4,5];
+//   GeeksForGeeks.forEach((var num)=> print(num));
+// var GeeksForGeeks1 = 4;
+//     int i = 1;
+//     while (i <= GeeksForGeeks1) {
+//         print('Hello Geek');
+//         i++;
+//     }
+//  var GeeksForGeeks2 = 4;
+//     int g = 1;
+//     do {
+//         print('Hello Geek');
+//         g++;
+//     } while (i <= GeeksForGeeks2);
+// var abc = ["Bilal","Umair","Hassan"];
+// for (var c in abc){
+//   print(c[0]);
+//   print(c);
+// }
+//  int b=1;
 
 
-}
+//   while(b<=5 ){
+
+//     print("int ${b}");
+
+//     b++;
+
+//   }
+  //  var num = 5; 
+  //  var factorial = 1; 
+   
+  //  while(num >=1) { 
+  //     factorial = factorial * num; 
+  //     num--; 
+  //  } 
+  //  print("The factorial  is ${factorial}");
+
+  // var num1 = int.parse(stdin.readLineSync()!);
+  // var num2 = int.parse(stdin.readLineSync()!);
+  
+  // print(num1+num2);
+// var email = stdin.readLineSync();
+// var password = stdin.readLineSync();
+
+// var isloogedin = false;
+//  while (isloogedin == false) {
+//   var email = stdin.readLineSync();
+// var password = stdin.readLineSync();
+// for (var i = 1; i <= 5; i++) {
+//   if (email == i && password == i) {
+    
+//   }
+// }
+//   if (email == "Umair@gmail.com" && password == "1122" ) {
+//     print("Login Successfully");
+//     isloogedin = true;
+//   }
+//   else(print("try Again"));
+//   // isloogedin = true;
+// } 
+// var a = 0;
+// do {
+//   print(a);
+//   a++;
+// } // while (a>5); 
+//   while (a<5);
+// int n1=1;  
+// int n2=1;   
+//    // We are checking multiple condition by using logical operators.  
+//    while (n1 <= 4 && n2 <= 3)  
+//    {  
+//     print("n1 : ${n1}, n2: ${n2}");  
+//     n1++;  
+//     n2++;  
+  //  }  
+
+//    int i = 10;  
+// print("Dart do-while loop example");  
+   
+// do{  
+          
+          
+//         print(i);  
+//         i++;  
+  
+// }while(i<=20);  
+// print("The loop is terminated");  
+// bool check;  
+// check = 20>10;  
+// print("The statement is = ${check}");  
 
 
 
 
 
+// }
+// void main() {
+// // abc(25, 45, 45, 55, 67,sec : "A");
+// // abc(75, 85, 45, 65, 94);
+// // table(25);
+// // table(45);
+// studentdata(name: "Umair", per: 85,sec: "B" );
+// studentdata(name: "Umair", per: 85 ,sec: "C");
+// studentdata(name: "Umair", per: 85 );
+// studentdata(name: "Umair", per: 85 );
+// }
+// // abc(sub1,sub2,sub3,sub4,sub5,{sec = "C"}){
+// // // num abc(num sub1,num sub2,num sub3,num sub4,num sub5,String sec)
 
-}
+// // var obtain = sub1+sub2+sub3+sub4+sub5;
+// // var per = ((obtain/500)*100);
+// // print(per);}
+// // print("$sec  : $per");
+// // return per;
+
+// //  table (num){
+  
+// //   for (var i = 0; i <=10; i++) {
+// //     print(i * num) ;
+    
+// //   }
+// // }
+
+// studentdata ({required name,required per,sec = "A"}){
+//   print(name);
+//   print(per);
+//   print(sec);
+//   print("---------------")
+// }
+
+
+
+
